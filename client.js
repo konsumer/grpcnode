c8838030bb7c68b0129434aaba9977497916c082
David Konsumer <konsumer@jetboystudio.com>
2017-02-02 17:25:33 -0800

    added generate

--- client.js
+++ client.js
@@ -30,6 +30,22 @@ const ls = (protoFile) => {
   return info
 }
 
+const generate = (protoFile) => {
+  const proto = grpc.load(protoFile)
+  let out = ''
+  Object.keys(proto).forEach(ns => {
+    Object.keys(proto[ns]).forEach(svc => {
+      if (proto[ns][svc].service) {
+        out += `module.exports.${svc} = {}\n`
+        proto[ns][svc].service.children.forEach(r => {
+          out += `\nmodule.exports.${svc}.${r.name} = (ctx, cb) => {\n  // do stuff with ctx.request\n  // cb(err, response)\n}\n`
+        })
+      }
+    })
+  })
+  return out
+}
+
 const main = () => {
   const argv = yargs
     .usage('Usage: $0 <command> [proto-file] [options]')
@@ -38,6 +54,7 @@ const main = () => {
     .example('$0 run -?', 'Get help on running an RPC method')
     .example(`$0 run example.proto -m helloworld.Greeter.sayHello -a '{"name": "World"}'`, 'Run helloworld.Greeter.sayHello({name:"World"}) from a server at localhost:5051, defined by api.proto')
     .example('$0 ls api.proto', 'List available RPC methods, defined by api.proto')
+    .example('$0 generate api.proto', 'Generate a server implementation stub from api.proto')
 
     .command('run [proto-file]', 'Run an RPC command', {
       host: {
@@ -55,8 +72,8 @@ const main = () => {
         alias: 'm'
       }
     })
-
     .command('ls [proto-file]', 'List available RPC commands')
+    .command('generate [proto-file]', 'Output a stub implementation for grpc-server.')
 
     .argv
 
@@ -66,33 +83,37 @@ const main = () => {
     process.exit(1)
   }
 
-  if (['ls', 'run'].indexOf(argv._[0]) === -1) {
-    console.log('Valid commands are `ls` and `run`.\n')
-    yargs.showHelp()
-    process.exit(1)
-  }
-
-  if (argv._[0] === 'run') {
-    run(argv.protoFile, argv.host, argv.method, argv.arguments ? JSON.parse(argv.arguments) : undefined)
-      .then(r => { console.log(JSON.stringify(r, null, 2)) })
-      .catch(e => {
-        console.error(e)
-        process.exit(1)
+  switch (argv._[0]) {
+    case 'run':
+      run(argv.protoFile, argv.host, argv.method, argv.arguments ? JSON.parse(argv.arguments) : undefined)
+        .then(r => { console.log(JSON.stringify(r, null, 2)) })
+        .catch(e => {
+          console.error(e)
+          process.exit(1)
+        })
+      break
+    case 'ls':
+      const info = ls(argv.protoFile)
+      console.log('Methods:')
+      Object.keys(info.services).forEach(ns => {
+        info.services[ns].forEach(method => {
+          console.log(`  ${ns}.${method}`)
+        })
       })
-  } else {
-    const info = ls(argv.protoFile)
-    console.log('Methods:')
-    Object.keys(info.services).forEach(ns => {
-      info.services[ns].forEach(method => {
-        console.log(`  ${ns}.${method}`)
+      console.log('\nMessage-types:')
+      Object.keys(info.messages).forEach(msg => {
+        console.log(`  ${msg}: ${info.messages[msg]}`)
       })
-    })
-    console.log('\nMessage-types:')
-    Object.keys(info.messages).forEach(msg => {
-      console.log(`  ${msg}: ${info.messages[msg]}`)
-    })
+      break
+    case 'generate':
+      console.log(generate(argv.protoFile))
+      break
+    default:
+      console.log('Valid commands are `ls`, `generate`, and `run`.\n')
+      yargs.showHelp()
+      process.exit(1)
   }
 }
 
-module.exports = {run, ls}
+module.exports = {run, ls, generate}
 if (require.main === module) { main() }
